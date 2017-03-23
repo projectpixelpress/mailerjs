1cf123e8e329f0511de51abecd02c89e1f479a61
Evan <evan@projectpixelpress.com>
2017-03-23 15:57:39 -0500

    split out and wrote tests for send_ses_to

--- src/mailer.js
+++ src/mailer.js
@@ -1,42 +1,24 @@
-var nodemailer = require('nodemailer'),
-    fs = require('fs'),
-    moment = require('moment'),
-    mandrill = require('mandrill-api'),
-    templatePath = 'lib/html',
-    S = require('string');
+var nodemailer = require('nodemailer');
 
 var smtpTransport = nodemailer.createTransport({
-    service: "SES-US-WEST-2",
+    service: 'SES-US-WEST-2',
     auth: {
-        user: "AKIAJJENTHZU7ZDJMNJQ",
-        pass: "Akn+PlfKgTVrbaTYmN6XRC6ZDirMIflyV4avbSfjtcS0"
+        user: 'AKIAJJENTHZU7ZDJMNJQ',
+        pass: 'Akn+PlfKgTVrbaTYmN6XRC6ZDirMIflyV4avbSfjtcS0'
     }
-    //"SMTP", {
-    //service: "Gmail",
+    //'SMTP', {
+    //service: 'Gmail',
     //auth: {
-    //user: "share@projectpixelpress.com",
-    //pass: "Ridley888786"
+    //user: 'share@projectpixelpress.com',
+    //pass: 'Ridley888786'
     //}
 });
-
-class emailMessage {
-    constructor(args) {
-        for (var property in args) {
-            this[property] = args[property];
-        }
-        this.from = args.from || "noreply@projectpixelpress.com";
-        this.to = args.to || null;
-        this.subject = args.subject || "message from ProjectPixelPress";
-        this.html = args.html || "<h1>aloha</h1>";
-        this.text = S(this.html).stripTags().s;
-    }
-}
 
 var sendMail = function(message) {
     if (!message) return false;
     smtpTransport.sendMail(message, function(error) {
         if (error) {
-            console.log("Error %o", error.message);
+            console.log('Error %o', error.message);
             smtpTransport.close();
             return;
         } else {
@@ -47,46 +29,6 @@ var sendMail = function(message) {
     });
 }
 
-var send_ses_to = function(recipient, options) {
-    //options = {template_name:'',subject:'',from:'',url_to_send:''}
-    //recipient = { name: 'Good sir', email: 'brad@projectpixelpress.com' }
-    if (options == null) options = {};
-    console.log('recipient: %o', recipient);
-    if (recipient == null || recipient.email == null) { // }|| recipient.name == null) {
-        console.log('using default recipient');
-        recipient = {
-            name: 'Good sir',
-            email: 'brad@projectpixelpress.com'
-        }
-    }
-    if (options.template_name == null) options.template_name = 'Verification';
-    if (options.userName == null) options.userName = 'New User';
-    if (options.subject == null) options.subject = 'Test subject';
-    if (options.from == null) options.from = 'support@bloxelsbuilder.com';
-    if (options.verifyCode == null) options.verifyCode = 'NONE';
-    if (options.url_to_send == null) options.url_to_send = 'http://bloxelsbuilder.com';
-
-    var admin_email = "brad@projectpixelpress.com";
-
-    var templateFile = fs.readFileSync(templatePath + "/" + options.template_name + ".html", "utf8");
-    //console.log(templateFile);
-    var body = templateFile.split("*|USERNAME|*").join(options.userName);
-    var body = body.split("*|ACTIVATION_CODE|*").join(options.verifyCode);
-    var body = body.split("*|CLICK_HERE|*").join(options.url_to_send);
-    var body = body.split("*|NAME_OF_RECIPIENT|*").join(recipient.name);
-
-    var message = new emailMessage({
-        "subject": options.subject,
-        "to": recipient.email,
-        "bcc": admin_email,
-        "html": body
-    });
-
-    sendMail(message);
-}
-
 module.exports = {
-    send_ses_to: send_ses_to,
-    sendMail: sendMail,
-    emailMessage: emailMessage
+    sendMail: sendMail
 };
---
+++ test/packagerSpec.js
@@ -0,0 +1,111 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+
+const mailer = require('./../src/mailer');
+
+describe('packager', () => {
+    let packager;
+    before(() => {
+        packager = require('./../src/packager');
+    });
+    describe('emailMessage', () => {
+        it('default values', () => {
+            const testObject = new packager.emailMessage({});
+
+            expect(testObject.from).to.equal('noreply@projectpixelpress.com');
+            expect(testObject.to).to.be.null;
+            expect(testObject.subject).to.equal('message from ProjectPixelPress');
+            expect(testObject.html).to.equal('<h1>aloha</h1>');
+            expect(testObject.text).to.equal('aloha');
+        });
+
+        it('can be overridden with real values', () => {
+            expected = {
+                from: 'test@example.com',
+                to: 'worstUser@anotherEmail.gov',
+                subject: 'Here\'s some mail!',
+                html: '<marquee>dummy</marquee>',
+                text: 'dummy',
+            };
+            const testObject = new packager.emailMessage(expected);
+
+            expect(testObject.from).to.equal(expected.from);
+            expect(testObject.to).to.equal(expected.to);
+            expect(testObject.subject).to.equal(expected.subject);
+            expect(testObject.html).to.equal(expected.html);
+            expect(testObject.text).to.equal(expected.text);
+        });
+
+        it('adds everything w/o a default', () => {
+            expected = {
+                unexpected: 'spanish inquisition'
+            };
+            const testObject = new packager.emailMessage(expected);
+
+            expect(testObject.unexpected).to.equal(expected.unexpected);
+        });
+    });
+    describe('send_ses_to', () => {
+        beforeEach(() => {
+            sinon.stub(mailer, 'sendMail');
+        });
+        afterEach(() => {
+            mailer.sendMail.restore();
+        });
+
+        it('defaults', () => {
+            const expectedBody = 'a very hot body';
+
+            packager.send_ses_to(null, null, expectedBody);
+
+            expect(mailer.sendMail.calledOnce).to.be.true;
+            actual = mailer.sendMail.getCall(0).args[0];
+            expect(actual.subject).to.equal('Test subject');
+            expect(actual.html).to.equal(expectedBody);
+            expect(actual.to).to.equal('brad@projectpixelpress.com');
+        });
+
+        it('bccs the admin', () => {
+          const expectedBody = 'a very hot body';
+
+          packager.send_ses_to(null, null, expectedBody);
+
+          expect(mailer.sendMail.calledOnce).to.be.true;
+          actual = mailer.sendMail.getCall(0).args[0];
+          expect(actual.bcc).to.equal('brad@projectpixelpress.com');
+        });
+
+        it('will not send without a body', () => {
+            packager.send_ses_to(null, null, null);
+            packager.send_ses_to();
+            packager.send_ses_to({}, {});
+
+            expect(mailer.sendMail.callCount).to.equal(0);
+        });
+
+        it('reads from recipient for email', () => {
+            const expectedEmail = 'anEmail@totally.org';
+
+            packager.send_ses_to({
+                email: expectedEmail
+            }, null, 'a body');
+
+            expect(mailer.sendMail.calledOnce).to.be.true;
+            actual = mailer.sendMail.getCall(0).args[0];
+            expect(actual.to).to.equal(expectedEmail);
+        });
+
+        it('reads from options for subject', () => {
+            const expectedSubject = 'Subject to further investigation';
+
+            packager.send_ses_to(null, {
+                subject: expectedSubject
+            }, 'a body');
+
+            expect(mailer.sendMail.calledOnce).to.be.true;
+            actual = mailer.sendMail.getCall(0).args[0];
+            expect(actual.subject).to.equal(expectedSubject);
+        });
+    });
+});
