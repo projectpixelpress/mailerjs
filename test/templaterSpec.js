fb1fa80f28594858e57a70c8b2387d6eefb3374c
Evan <evan@projectpixelpress.com>
2017-03-23 17:16:17 -0500

    completely reworking templater

--- test/templaterSpec.js
+++ test/templaterSpec.js
@@ -9,64 +9,23 @@ describe('templater', () => {
     beforeEach(() => {
         templater = require('./../src/templater');
     });
+
     describe('parse_template', () => {
-        describe('with no templatePath', () => {
-            it('does no work', () => {
-                expect(templater.parse_template.bind()).to.throw('Cannot null/empty parse template');
-            });
+        it('with no templatePath does no work', () => {
+            expect(templater.parse_template.bind()).to.throw('Cannot null/empty parse template');
         });
 
-        describe('with templatePath', () => {
-            it('replaces variables', () => {
-                const variables = [
-                    '*|USERNAME|*',
-                    '*|ACTIVATION_CODE|*',
-                    '*|CLICK_HERE|*',
-                    '*|NAME_OF_RECIPIENT|*',
-                ];
+        it('with templatePath replaces variables', () => {
+            let replacements = {};
+            replacements['*|USERNAME|*'] = 'userName';
+            replacements['*|ACTIVATION_CODE|*'] = 'verifyCode';
+            replacements['*|CLICK_HERE|*'] = 'url_to_send';
+            replacements['*|NAME_OF_RECIPIENT|*'] = 'name';
+            const template = 'some text *|USERNAME|* *|ACTIVATION_CODE|**|ACTIVATION_CODE|**|NAME_OF_RECIPIENT|**|ACTIVATION_CODE|* *|CLICK_HERE|* *|NAME_OF_RECIPIENT|*';
 
-                const actual = templater.parse_template({}, {}, 'some text *|USERNAME|* *|ACTIVATION_CODE|**|ACTIVATION_CODE|**|NAME_OF_RECIPIENT|**|ACTIVATION_CODE|* *|CLICK_HERE|* *|NAME_OF_RECIPIENT|*');
+            const actual = templater.parse_template(template, replacements);
 
-                for (variable of variables) {
-                    expect(actual.includes(variable)).to.be.false;
-                }
-            });
-
-            it('default values', () => {
-                const actual = templater.parse_template({}, {}, '*|USERNAME|* *|NAME_OF_RECIPIENT|* *|ACTIVATION_CODE|* *|CLICK_HERE|*');
-
-                expect(actual).to.equal('New User Good sir NONE http://bloxelsbuilder.com');
-            });
-
-            it('defaults recipient name if it is not set ', () => {
-                const actual = templater.parse_template({
-                    email: 'who@cares.com'
-                }, {}, '*|NAME_OF_RECIPIENT|*');
-
-                expect(actual).to.equal('Good sir');
-            });
-
-            it('reads name from recipient', () => {
-                const expectedName = "testy mcTesterson";
-                const actual = templater.parse_template({
-                    name: expectedName
-                }, {}, '*|NAME_OF_RECIPIENT|*');
-
-                expect(actual).to.equal(expectedName);
-            });
-
-            it('reads things from options', () => {
-                const expectedUsername = "testy_mcTesterson";
-                const expectedVerifyCode = "verified";
-                const expectedUrlToSend = "http://example.co";
-                const actual = templater.parse_template({}, {
-                    userName: expectedUsername,
-                    verifyCode: expectedVerifyCode,
-                    url_to_send: expectedUrlToSend,
-                }, '*|USERNAME|**|ACTIVATION_CODE|**|CLICK_HERE|*');
-
-                expect(actual).to.equal(expectedUsername + expectedVerifyCode + expectedUrlToSend);
-            });
+            expect(actual).to.equal('some text userName verifyCodeverifyCodenameverifyCode url_to_send name');
         });
     });
 });
