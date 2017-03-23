7dccad8b51e5c9ab00ad5f9e8c1ae2aca7f41654
Evan <evan@projectpixelpress.com>
2017-03-23 16:58:43 -0500

    you need to pass in text now, not just a path to a template (do that in your project)

--- test/templaterSpec.js
+++ test/templaterSpec.js
@@ -5,45 +5,27 @@ const sinon = require('sinon');
 const fs = require('fs');
 
 describe('templater', () => {
+    let templater;
+    beforeEach(() => {
+        templater = require('./../src/templater');
+    });
     describe('parse_template', () => {
         describe('with no templatePath', () => {
-          let templater;
-          let stubbedRead;
-          beforeEach(() => {
-              templater = require('./../src/templater');
-              stubbedRead = sinon.stub(fs, 'readFileSync')
-          });
-          afterEach(() => {
-              fs.readFileSync.restore();
-          });
             it('does no work', () => {
-                expect(templater.parse_template.bind()).to.throw('Cannot parse template without knowing where your templates are');
-                expect(fs.readFileSync.callCount).to.equal(0);
+                expect(templater.parse_template.bind()).to.throw('Cannot null/empty parse template');
             });
         });
 
         describe('with templatePath', () => {
-            const templatePath = 'testTemplatePath';
-            let templater;
-            let stubbedRead;
-            beforeEach(() => {
-                templater = require('./../src/templater');
-                stubbedRead = sinon.stub(fs, 'readFileSync')
-                stubbedRead.returns('some text *|USERNAME|* *|ACTIVATION_CODE|**|ACTIVATION_CODE|**|NAME_OF_RECIPIENT|**|ACTIVATION_CODE|* *|CLICK_HERE|* *|NAME_OF_RECIPIENT|*');
-            });
-            afterEach(() => {
-                fs.readFileSync.restore();
-            });
-
             it('replaces variables', () => {
                 const variables = [
-                  '*|USERNAME|*',
-                  '*|ACTIVATION_CODE|*',
-                  '*|CLICK_HERE|*',
-                  '*|NAME_OF_RECIPIENT|*',
+                    '*|USERNAME|*',
+                    '*|ACTIVATION_CODE|*',
+                    '*|CLICK_HERE|*',
+                    '*|NAME_OF_RECIPIENT|*',
                 ];
 
-                const actual = templater.parse_template({}, {}, templatePath);
+                const actual = templater.parse_template({}, {}, 'some text *|USERNAME|* *|ACTIVATION_CODE|**|ACTIVATION_CODE|**|NAME_OF_RECIPIENT|**|ACTIVATION_CODE|* *|CLICK_HERE|* *|NAME_OF_RECIPIENT|*');
 
                 for (variable of variables) {
                     expect(actual.includes(variable)).to.be.false;
@@ -51,37 +33,37 @@ describe('templater', () => {
             });
 
             it('default values', () => {
-                stubbedRead.returns('*|USERNAME|* *|NAME_OF_RECIPIENT|* *|ACTIVATION_CODE|* *|CLICK_HERE|*');
-                const actual = templater.parse_template({}, {}, templatePath);
+                const actual = templater.parse_template({}, {}, '*|USERNAME|* *|NAME_OF_RECIPIENT|* *|ACTIVATION_CODE|* *|CLICK_HERE|*');
 
                 expect(actual).to.equal('New User Good sir NONE http://bloxelsbuilder.com');
             });
 
             it('defaults recipient name if it is not set ', () => {
-                stubbedRead.returns('*|NAME_OF_RECIPIENT|*');
-                const actual = templater.parse_template({email: 'who@cares.com'}, {}, templatePath);
+                const actual = templater.parse_template({
+                    email: 'who@cares.com'
+                }, {}, '*|NAME_OF_RECIPIENT|*');
 
                 expect(actual).to.equal('Good sir');
             });
 
             it('reads name from recipient', () => {
-                stubbedRead.returns('*|NAME_OF_RECIPIENT|*');
                 const expectedName = "testy mcTesterson";
-                const actual = templater.parse_template({name: expectedName}, {}, templatePath);
+                const actual = templater.parse_template({
+                    name: expectedName
+                }, {}, '*|NAME_OF_RECIPIENT|*');
 
                 expect(actual).to.equal(expectedName);
             });
 
             it('reads things from options', () => {
-                stubbedRead.returns('*|USERNAME|**|ACTIVATION_CODE|**|CLICK_HERE|*');
                 const expectedUsername = "testy_mcTesterson";
                 const expectedVerifyCode = "verified";
                 const expectedUrlToSend = "http://example.co";
                 const actual = templater.parse_template({}, {
-                  userName: expectedUsername,
-                  verifyCode: expectedVerifyCode,
-                  url_to_send: expectedUrlToSend,
-                }, templatePath);
+                    userName: expectedUsername,
+                    verifyCode: expectedVerifyCode,
+                    url_to_send: expectedUrlToSend,
+                }, '*|USERNAME|**|ACTIVATION_CODE|**|CLICK_HERE|*');
 
                 expect(actual).to.equal(expectedUsername + expectedVerifyCode + expectedUrlToSend);
             });
