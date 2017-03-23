94d506c580b9f1c9d03d5d3cdfd8dce75252ce05
Evan <evan@projectpixelpress.com>
2017-03-23 11:58:02 -0500

    adding more smoke tests

--- test/smoketest.js
+++ test/smoketest.js
@@ -5,6 +5,29 @@ var expect = chai.expect;
 describe('mailerjs', function() {
   it('should be import-able', function() {
     var testObject = require('./../src/mailer');
-    expect(testObject).to.exist;
+    expect(testObject).to.be.exist;
+  });
+
+  describe('once imported', function() {
+    var mailerjs;
+    before(function() {
+      mailerjs = require('./../src/mailer');
+    });
+
+    it('should export a function called send_mandrill_to', function() {
+      expect(mailerjs.send_mandrill_to).to.be.exist;
+    });
+
+    it('should export a function called send_ses_to', function() {
+      expect(mailerjs.send_ses_to).to.be.exist;
+    });
+
+    it('should export a function called sendMail', function() {
+      expect(mailerjs.sendMail).to.be.exist;
+    });
+
+    it('should export a function called emailMessage', function() {
+      expect(mailerjs.emailMessage).to.be.exist;
+    });
   });
 });
