f7cd65d083300a01327e2773a0d3b077c12c5637
Evan <evan@projectpixelpress.com>
2017-03-23 17:21:08 -0500

    beautifying code

--- test/smoketest.js
+++ test/smoketest.js
@@ -3,31 +3,31 @@ const expect = chai.expect;
 
 
 describe('smoketest - mailerjs', function() {
-  it('should be import-able', function() {
-    const testObject = require('./../index');
-    expect(testObject).to.be.exist;
-  });
-
-  describe('once imported', function() {
-    let mailerjs;
-    before(function() {
-      mailerjs = require('./../index');
+    it('should be import-able', function() {
+        const testObject = require('./../index');
+        expect(testObject).to.be.exist;
     });
 
-    it('should export a function called send_ses_to', function() {
-      expect(mailerjs.send_ses_to).to.be.exist;
-    });
+    describe('once imported', function() {
+        let mailerjs;
+        before(function() {
+            mailerjs = require('./../index');
+        });
 
-    it('should export a function called sendMail', function() {
-      expect(mailerjs.sendMail).to.be.exist;
-    });
+        it('should export a function called send_ses_to', function() {
+            expect(mailerjs.send_ses_to).to.be.exist;
+        });
 
-    it('should export a class called emailMessage', function() {
-      expect(mailerjs.emailMessage).to.be.exist;
-    });
+        it('should export a function called sendMail', function() {
+            expect(mailerjs.sendMail).to.be.exist;
+        });
 
-    it('should export a function called parse_template', function() {
-      expect(mailerjs.parse_template).to.be.exist;
+        it('should export a class called emailMessage', function() {
+            expect(mailerjs.emailMessage).to.be.exist;
+        });
+
+        it('should export a function called parse_template', function() {
+            expect(mailerjs.parse_template).to.be.exist;
+        });
     });
-  });
 });
