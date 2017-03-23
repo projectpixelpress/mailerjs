5162588e6e5b72db438768b3075f57cf5c0f8e08
Evan <evan@projectpixelpress.com>
2017-03-23 17:16:17 -0500

    completely reworking templater

--- src/templater.js
+++ src/templater.js
@@ -1,25 +1,14 @@
-const parse_template = function(recipient, options, template) {
+const parse_template = function(template, replacements) {
   if (!template) {
     console.log('Cannot null/empty parse template');
     throw new Error('Cannot null/empty parse template');
   }
 
-  console.log('recipient: %o', recipient);
-  if (recipient == null || recipient.name == null) {
-      console.log('using default recipient');
-      recipient = {
-          name: 'Good sir'
-      }
+  let body = template;
+  for (replacementKey in replacements) {
+    const replaceWith = replacements[replacementKey];
+    body = body.split(replacementKey).join(replaceWith);
   }
-  options = options || {};
-  options.userName = options.userName || 'New User';
-  options.verifyCode = options.verifyCode || 'NONE';
-  options.url_to_send = options.url_to_send || 'http://bloxelsbuilder.com';
-
-  let body = template.split('*|USERNAME|*').join(options.userName);
-  body = body.split('*|ACTIVATION_CODE|*').join(options.verifyCode);
-  body = body.split('*|CLICK_HERE|*').join(options.url_to_send);
-  body = body.split('*|NAME_OF_RECIPIENT|*').join(recipient.name);
 
   return body;
 }
