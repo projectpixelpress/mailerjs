405b7ea104cbed9ecc5f0db4c8c3e794fac1eac7
Bradley McCrorey <brad@projectpixelpress.com>
2017-03-28 16:28:43 -0500

    works with extra params - need to implement checks to actually specify transport

--- src/mailer.js
+++ src/mailer.js
@@ -1,34 +1,29 @@
 var nodemailer = require('nodemailer');
 
-var smtpTransport = nodemailer.createTransport({
-    service: 'SES-US-WEST-2',
-    auth: {
-        user: 'AKIAJJENTHZU7ZDJMNJQ',
-        pass: 'Akn+PlfKgTVrbaTYmN6XRC6ZDirMIflyV4avbSfjtcS0'
-    }
-    //'SMTP', {
-    //service: 'Gmail',
-    //auth: {
-    //user: 'share@projectpixelpress.com',
-    //pass: 'Ridley888786'
-    //}
-});
-
-var sendMail = function(message) {
-    if (!message) return false;
-    smtpTransport.sendMail(message, function(error) {
-        if (error) {
-            console.log('Error %o', error.message);
-            smtpTransport.close();
-            return;
-        } else {
-            console.log('Message sent successfully!');
-            smtpTransport.close();
-            return;
+var smtpTransport = nodemailer.createTransport(
+    {
+        service: "SES-US-WEST-2",
+        auth: {
+            user: "AKIAJJENTHZU7ZDJMNJQ",
+            pass: "Akn+PlfKgTVrbaTYmN6XRC6ZDirMIflyV4avbSfjtcS0"
         }
-    });
-}
+    }
+);
 
 module.exports = {
-    sendMail: sendMail
+    smtpTransport: smtpTransport,
+    sendMail: function(message) {
+        if (!message) return false;
+        this.smtpTransport.sendMail(message, function(error) {
+            if (error) {
+                console.log('Error %o', error.message);
+                smtpTransport.close();
+                return;
+            } else {
+                console.log('Message sent successfully!');
+                smtpTransport.close();
+                return;
+            }
+        });
+    }
 };
