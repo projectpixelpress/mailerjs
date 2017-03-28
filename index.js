13266fbef541d8796adb26227205f01489bd2680
Bradley McCrorey <brad@projectpixelpress.com>
2017-03-28 16:28:43 -0500

    works with extra params - need to implement checks to actually specify transport

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const mailer = require('./src/mailer');
 const packager = require('./src/packager');
 const templater = require('./src/templater');
+var nodemailer = require('nodemailer');
 
-module.exports = Object.assign(mailer, packager, templater);
+const extraParams = function() {
+    console.log("extra params: %o",arguments);
+
+    delete mailer.smtpTransport;
+    mailer.smtpTransport = nodemailer.createTransport(
+        "SMTP", {
+            service: "Gmail",
+            auth: {
+                user: "share@projectpixelpress.com",
+                pass: "***REMOVED***"
+            }
+        }
+    );
+
+    return Object.assign(extraParams, mailer, packager, templater);
+}
+module.exports = Object.assign(extraParams, mailer, packager, templater);
