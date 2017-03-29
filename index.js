9481377710ac47a74faee62362e0e89dc56fd338
Bradley McCrorey <brad@projectpixelpress.com>
2017-03-29 16:59:23 -0500

    require username/password in constructor

--- index.js
+++ index.js
@@ -6,18 +6,32 @@ var nodemailer = require('nodemailer');
 const extraParams = function() {
     console.log("extra params: %o",arguments);
 
-    if(!!arguments && arguments[0]["transport"] && arguments[0]["transport"] === "gmail") {
-        console.log("changing to gmail");
-        delete mailer.smtpTransport;
-        mailer.smtpTransport = nodemailer.createTransport(
-            {
-                service: "Gmail",
-                auth: {
-                    user: process.env.GMAIL_USER,
-                    pass: process.env.GMAIL_PASS
+    if(!!arguments && !!arguments[0]["username"] && !!arguments[0]["password"]) {
+        if(!!arguments && arguments[0]["transport"] && arguments[0]["transport"] === "gmail") {
+            console.log("changing to gmail");
+            delete mailer.smtpTransport;
+            mailer.smtpTransport = nodemailer.createTransport(
+                {
+                    service: "Gmail",
+                    auth: {
+                        user: arguments[0]["username"],
+                        pass: arguments[0]["password"]
+                    }
                 }
-            }
-        );
+            );
+        } else {
+            mailer.smtpTransport = nodemailer.createTransport(
+                {
+                    service: "SES-US-WEST-2",
+                    auth: {
+                        user: arguments[0]["username"],
+                        pass: arguments[0]["password"]
+                    }
+                }
+            );
+        }
+    } else {
+        throw new error("need to supply (at least) username and password in constructor");
     }
 
     return Object.assign(extraParams, mailer, packager, templater);
