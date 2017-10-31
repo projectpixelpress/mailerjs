7a3a836c516cf3ceb75b1e1a47ba3ac061a4fc68
Bradley McCrorey <brad@projectpixelpress.com>
2017-10-31 10:52:57 -0500

    fixed params

--- src/verifier.js
+++ src/verifier.js
@@ -2,15 +2,14 @@ const emailVerify = require('email-verify');
 
 module.exports = {
     verify_email: (email) => {
+        let verifyOptions = {
+            port : 25,
+            sender : 'share@projectpixelpress.com',
+            fqdn : 'smtp.gmail.com',
+            debug: true
+        };
         return new Promise((resolve, reject) => {
-            let verifyOptions = {
-                port : 25,
-                sender : 'share@projectpixelpress.com',
-                fqdn : 'smtp.gmail.com',
-                debug: true
-            };
-            let individualOptions = Object.assign({email:email},verifyOptions);
-            emailVerify.verify(individualOptions, null, function(err, info) {
+            emailVerify.verify(email, verifyOptions, function(err, info) {
                 console.log("info: %o", info);
                 console.log("err: %o", err);
                 if (err) {
